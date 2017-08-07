6d45ca39e8495610a06466dc16a8ddad7a14fa7b
Alinson Xavier <axavier@uwaterloo.ca>
2017-08-07 09:39:13 -0400

    Update CPLEX path

--- cmake/FindCPLEX.cmake
+++ cmake/FindCPLEX.cmake
@@ -1,4 +1,5 @@
 set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH}
+  /opt/cplex-12.6/cplex/lib/x86-64_linux/static_pic
   /software/cplex-12.6/distribution/cplex/lib/x86-64_linux/static_pic
   /Users/axavier/Applications/IBM/ILOG/CPLEX_Studio1262/cplex/lib/x86-64_osx/static_pic)
 
@@ -7,7 +8,8 @@ find_library(CPLEX_LIBRARIES
 
 find_path(CPLEX_INCLUDE_DIR NAMES ilcplex/cplex.h PATHS
   /Users/axavier/Applications/IBM/ILOG/CPLEX_Studio1262/cplex/include
-  /software/cplex-12.6/distribution/cplex/include/)
+  /software/cplex-12.6/distribution/cplex/include/
+  /opt/cplex-12.6/cplex/include/)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
